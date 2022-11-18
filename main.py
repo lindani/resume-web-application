5d5c1e6dfd0d1d0f56d0c7d8fc8801f8afcf2052
lindani <panilindani02@gmail.com>
2022-11-18 11:11:03 +0200

    feat: read cv contents and print results

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from PyPDF2 import PdfReader
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.get("/")
-def index():
-  return '<h1>Hello, World!</h1>'
+@app.route("/")
+def home():
+	reader = PdfReader("Lindani Pani CV - October_2022.pdf")
+	text = ""
+	for page in reader.pages:
+		text += page.extract_text() + "\n"
+	print(text.split('\n'))
+	# pdfFileObj.close() 
+	
+
+	return render_template('home.html')
+
 
 if __name__ == "__main__":
-    # Used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host="localhost", port=8080, debug=True)
+	# Used when running locally only. When deploying to Google App
+	# Engine, a webserver process such as Gunicorn will serve the app. This
+	# can be configured by adding an `entrypoint` to app.yaml.
+	app.run(debug=True)
